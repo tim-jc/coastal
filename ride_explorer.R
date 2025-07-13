45fb0641c37a88201bdc74a6b1ab6f263f4ca688
tim-jc <tim_cross@me.com>
2025-07-13 11:13:23 +0100

    .

---
+++ ride_explorer.R
@@ -0,0 +1,35 @@
+# ride explorer
+# use this script to add new rides to the coastal dataset
+
+# libraries
+library(tidyverse)
+library(leaflet)
+library(stravR)
+
+# clear the memory
+rm(list=ls(all = TRUE))
+
+source("coastal_vis_functions.R")
+source("config.R")
+
+
+# Load ride ---------------------------------------------------------------
+
+ride_id <- 15095197221
+
+ride_data <- tbl(con, "streams") %>% filter(strava_id == ride_id) %>% collect()
+
+max(ride_data$time)
+
+
+# Draw track, and adjust --------------------------------------------------
+
+# Adjust time filter to crop ride to correct size. Add start and end time
+# values to coastal_activities in functions script
+
+leaflet() %>% 
+  addTiles() %>% 
+  add_track(ride_data %>% filter(time >= 17992,
+                                 time <= 17994),
+            track_colour = "blue")
+
